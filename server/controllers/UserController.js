1495b46a05fe5ccc9fe79f290e440373524cccd2
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-07-05 15:55:38 -0500

    api routes created

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,7 +7,7 @@ router.get('/', function(request, response){
 	response.render('login')
 });
 
-router.post('/', function(request, response) {
+router.post('/profile', function(request, response) {
 // 1. find the user with the corresponding email address
   // 2. check if the password on that user matches
   //    the password from the request (hashed)
@@ -32,6 +32,25 @@ router.get('/register', function(request, response){
 	response.render('register')
 });
 
+////////////////////////////////////////
+////////////////////////////////////////
+////////////// JSON API ////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+
+router.get('/all', function(request, response){
+	User.find(function(error, users) {
+		response.json(users)
+	})
+})
+
+router.get('/:id', function(request, response) {
+	var id = request.params.id
+	User.findById(function(error, user) {
+		response.json(user)
+	})
+})
+
 router.post('/register', function(request, response){
 	bcrypt.hash(request.body.password, 10, function(error, hash){
 		var user = new User({firstname: request.body.firstname,
@@ -47,6 +66,29 @@ router.post('/register', function(request, response){
 	response.json(user);
 	})
 });
+
+router.patch('/:id', function(request, response) {
+	var id = request.params.id
+	User.findById(function(error, user) {
+		user.firstname = request.body.firstname;
+		user.lastname = request.body.lastname;
+		user.username = request.body.username;
+		user.password = request.body.password;
+		user.city = request.body.city;
+		user.state = request.body.state;
+		user.gender = request.body.gender;
+		user.pictureurl = request.body.pictureurl;
+		user.save()
+		response.json(user)
+	})
+})
+
+router.delete('/:id', function(request, response) {
+	var id = request.params.id
+	User.findById(function(error, user) {
+		user.delete();
+	})
+})
 
 module.exports = router;
 
