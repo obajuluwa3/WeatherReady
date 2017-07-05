b19e1b9fb3561d7b54e521854332e0960e02d4b4
Odunayo Obajuluwa <odunayoobajuluwa@Odunayos-MacBook-Pro.local>
2017-07-05 15:31:35 -0500

    working register and login pages

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -4,12 +4,38 @@ var User = require('../models/User');
 var bcrypt = require('bcrypt');
 
 router.get('/', function(request, response){
+	response.render('login')
+});
+
+router.post('/', function(request, response) {
+// 1. find the user with the corresponding email address
+  // 2. check if the password on that user matches
+  //    the password from the request (hashed)
+  User.findOne({username: request.body.username}, function(err, user){
+    // check if there is a user that was returned from the DB
+    if(user){
+      bcrypt.compare(request.body.password, user.password, function(error, match){
+        if(match === true){
+          // request.session.loggedIn = true;
+          response.redirect("/profile" + user._id);
+        }else{
+          response.redirect('/users');
+        }
+      })
+    }else{
+      response.redirect('/users');
+    }
+  })
+})
+
+router.get('/register', function(request, response){
 	response.render('register')
 });
 
-router.post('/', function(request, response){
+router.post('/register', function(request, response){
 	bcrypt.hash(request.body.password, 10, function(error, hash){
-		var user = new User({name: request.body.name,
+		var user = new User({firstname: request.body.firstname,
+								lastname: request.body.lastname,
 								username: request.body.username,
 								password: hash,
 								city: request.body.city,
--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-	name: String,
+	firstname: String,
+	lastname: String,
 	username: String,
 	password: String,
 	city: String,
